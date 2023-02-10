5157b4760bb19c958abb107cf346b1300ffafafb
siixty <melosbobi11@gmail.com>
2023-02-10 20:10:30 +0100

    first commit

---
+++ Calculator.java
@@ -0,0 +1,458 @@
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.util.regex.Pattern;
+import java.awt.Color;
+import javax.swing.*;
+import java.lang.Math;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Calculator {
+
+    private static final int WINDOW_WIDTH = 403;
+    private static final int WINDOW_HEIGHT = 600;
+    private static final int BUTTON_WIDTH = 80;
+    private static final int BUTTON_HEIGHT = 70;
+    private static final int MARGIN_X = 20;
+    private static final int MARGIN_Y = 60;
+
+    private JFrame window; // Main window
+    private JTextField inText; // Input
+    private RoundedButton btnC, btnBack, btnMod, btnDiv, btnMul, btnSub, btnAdd,
+            btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,
+            btnPoint, btnEqual, alwaysOnTop;
+
+    private char opt = ' '; // Save the operator
+    private boolean go = true; // For calculate with Opt != (=)
+    private boolean addWrite = true; // Connect numbers in display
+    private double val = 0; // Save the value typed for calculation
+
+    public Calculator() {
+
+        window = new JFrame("Calculator");
+        window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        window.setLocationRelativeTo(null); // Move window to center
+        final Color mainBack = new Color(32, 32, 32);
+        window.setBackground(mainBack);
+        ImageIcon img = new ImageIcon("icon/calculator.png");
+        window.setIconImage(img.getImage());
+
+        int[] x = { MARGIN_X, MARGIN_X + 90, 200, 290, 380 };
+        int[] y = { MARGIN_Y, MARGIN_Y + 100, MARGIN_Y + 180, MARGIN_Y + 260, MARGIN_Y + 340, MARGIN_Y + 420 };
+
+        inText = new JTextField("0");
+        inText.setBounds(x[0], y[0], 350, 70);
+        inText.setBackground(mainBack);
+
+        inText.setForeground(Color.white);
+        inText.setFont(new Font("Tahoma", Font.PLAIN, 33));
+        inText.setHorizontalAlignment(JTextField.RIGHT);
+        inText.setBorder(null);
+
+        KeyListener listener = new KeyListener() {
+            @Override
+            public void keyPressed(KeyEvent event) {
+                char keyCode = event.getKeyChar();
+
+                if (Character.isDigit(keyCode)) {
+                    if (inText.getText().equals(" ")) {
+                        inText.setText(String.valueOf(keyCode));
+                    } else {
+                        inText.setText(inText.getText() + keyCode);
+                    }
+                }
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+        };
+        window.addKeyListener(listener);
+
+        window.add(inText);
+
+        btnC = initBtn("C", x[0], y[1], event -> {
+            repaintFont();
+            inText.setText("0");
+            opt = ' ';
+            val = 0;
+        });
+
+        btnBack = initBtn("<-", x[1], y[1], event -> {
+            repaintFont();
+            String str = inText.getText();
+            StringBuilder str2 = new StringBuilder();
+            for (int i = 0; i < (str.length() - 1); i++) {
+                str2.append(str.charAt(i));
+            }
+            if (str2.toString().equals("")) {
+                inText.setText("0");
+            } else {
+                inText.setText(str2.toString());
+            }
+        });
+
+        btnMod = initBtn("%", x[2], y[1], event -> {
+            repaintFont();
+            if (Pattern.matches("([-]?\\d+[.]\\d*)|(\\d+)", inText.getText()))
+                if (go) {
+                    val = calc(val, inText.getText(), opt);
+                    if (Pattern.matches("[-]?[\\d]+[.][0]*", String.valueOf(val))) {
+                        inText.setText(String.valueOf((int) val));
+                    } else {
+                        inText.setText(String.valueOf(val));
+                    }
+                    opt = '%';
+                    go = false;
+                    addWrite = false;
+                }
+        });
+
+        btnDiv = initBtn("/", x[3], y[1], event -> {
+            repaintFont();
+            if (Pattern.matches("([-]?\\d+[.]\\d*)|(\\d+)", inText.getText()))
+                if (go) {
+                    val = calc(val, inText.getText(), opt);
+                    if (Pattern.matches("[-]?[\\d]+[.][0]*", String.valueOf(val))) {
+                        inText.setText(String.valueOf((int) val));
+                    } else {
+                        inText.setText(String.valueOf(val));
+                    }
+                    opt = '/';
+                    go = false;
+                    addWrite = false;
+                } else {
+                    opt = '/';
+                }
+        });
+
+        btn7 = initBtn("7", x[0], y[2], event -> {
+            repaintFont();
+            if (addWrite) {
+                if (Pattern.matches("[0]*", inText.getText())) {
+                    inText.setText("7");
+                } else {
+                    inText.setText(inText.getText() + "7");
+                }
+            } else {
+                inText.setText("7");
+                addWrite = true;
+            }
+            go = true;
+        });
+
+        btn8 = initBtn("8", x[1], y[2], event -> {
+            repaintFont();
+            if (addWrite) {
+                if (Pattern.matches("[0]*", inText.getText())) {
+                    inText.setText("8");
+                } else {
+                    inText.setText(inText.getText() + "8");
+                }
+            } else {
+                inText.setText("8");
+                addWrite = true;
+            }
+            go = true;
+        });
+
+        btn9 = initBtn("9", x[2], y[2], event -> {
+            repaintFont();
+            if (addWrite) {
+                if (Pattern.matches("[0]*", inText.getText())) {
+                    inText.setText("9");
+                } else {
+                    inText.setText(inText.getText() + "9");
+                }
+            } else {
+                inText.setText("9");
+                addWrite = true;
+            }
+            go = true;
+        });
+
+        btnMul = initBtn("*", x[3], y[2], event -> {
+            repaintFont();
+            if (Pattern.matches("([-]?\\d+[.]\\d*)|(\\d+)", inText.getText()))
+                if (go) {
+                    val = calc(val, inText.getText(), opt);
+                    if (Pattern.matches("[-]?[\\d]+[.][0]*", String.valueOf(val))) {
+                        inText.setText(String.valueOf((int) val));
+                    } else {
+                        inText.setText(String.valueOf(val));
+                    }
+                    opt = '*';
+                    go = false;
+                    addWrite = false;
+                } else {
+                    opt = '*';
+                }
+        });
+
+        btn4 = initBtn("4", x[0], y[3], event -> {
+            repaintFont();
+            if (addWrite) {
+                if (Pattern.matches("[0]*", inText.getText())) {
+                    inText.setText("4");
+                } else {
+                    inText.setText(inText.getText() + "4");
+                }
+            } else {
+                inText.setText("4");
+                addWrite = true;
+            }
+            go = true;
+        });
+
+        btn5 = initBtn("5", x[1], y[3], event -> {
+            repaintFont();
+            if (addWrite) {
+                if (Pattern.matches("[0]*", inText.getText())) {
+                    inText.setText("5");
+                } else {
+                    inText.setText(inText.getText() + "5");
+                }
+            } else {
+                inText.setText("5");
+                addWrite = true;
+            }
+            go = true;
+        });
+
+        btn6 = initBtn("6", x[2], y[3], event -> {
+            repaintFont();
+            if (addWrite) {
+                if (Pattern.matches("[0]*", inText.getText())) {
+                    inText.setText("6");
+                } else {
+                    inText.setText(inText.getText() + "6");
+                }
+            } else {
+                inText.setText("6");
+                addWrite = true;
+            }
+            go = true;
+        });
+
+        btnSub = initBtn("-", x[3], y[3], event -> {
+            repaintFont();
+            if (Pattern.matches("([-]?\\d+[.]\\d*)|(\\d+)", inText.getText()))
+                if (go) {
+                    val = calc(val, inText.getText(), opt);
+                    if (Pattern.matches("[-]?[\\d]+[.][0]*", String.valueOf(val))) {
+                        inText.setText(String.valueOf((int) val));
+                    } else {
+                        inText.setText(String.valueOf(val));
+                    }
+
+                    opt = '-';
+                    go = false;
+                    addWrite = false;
+                } else {
+                    opt = '-';
+                }
+        });
+        btn1 = initBtn("1", x[0], y[4], event -> {
+            repaintFont();
+            if (addWrite) {
+                if (Pattern.matches("[0]*", inText.getText())) {
+                    inText.setText("1");
+                } else {
+                    inText.setText(inText.getText() + "1");
+                }
+            } else {
+                inText.setText("1");
+                addWrite = true;
+            }
+            go = true;
+        });
+        btn2 = initBtn("2", x[1], y[4], event -> {
+            repaintFont();
+            if (addWrite) {
+                if (Pattern.matches("[0]*", inText.getText())) {
+                    inText.setText("2");
+                } else {
+                    inText.setText(inText.getText() + "2");
+                }
+            } else {
+                inText.setText("2");
+                addWrite = true;
+            }
+            go = true;
+        });
+        btn3 = initBtn("3", x[2], y[4], event -> {
+            repaintFont();
+            if (addWrite) {
+                if (Pattern.matches("[0]*", inText.getText())) {
+                    inText.setText("3");
+                } else {
+                    inText.setText(inText.getText() + "3");
+                }
+            } else {
+                inText.setText("3");
+                addWrite = true;
+            }
+            go = true;
+        });
+        btnAdd = initBtn("+", x[3], y[4], event -> {
+            repaintFont();
+            if (Pattern.matches("([-]?\\d+[.]\\d*)|(\\d+)", inText.getText()))
+                if (go) {
+                    val = calc(val, inText.getText(), opt);
+                    if (Pattern.matches("[-]?[\\d]+[.][0]*", String.valueOf(val))) {
+                        inText.setText(String.valueOf((int) val));
+                    } else {
+                        inText.setText(String.valueOf(val));
+                    }
+                    opt = '+';
+                    go = false;
+                    addWrite = false;
+                } else {
+                    opt = '+';
+                }
+        });
+        btnPoint = initBtn(".", x[0], y[5], event -> {
+            repaintFont();
+            if (addWrite) {
+                if (!inText.getText().contains(".")) {
+                    inText.setText(inText.getText() + ".");
+                }
+            } else {
+                inText.setText("0.");
+                addWrite = true;
+            }
+            go = true;
+        });
+        btn0 = initBtn("0", x[1], y[5], event -> {
+            repaintFont();
+            if (addWrite) {
+                if (Pattern.matches("[0]*", inText.getText())) {
+                    inText.setText("0");
+                } else {
+                    inText.setText(inText.getText() + "0");
+                }
+            } else {
+                inText.setText("0");
+                addWrite = true;
+            }
+            go = true;
+        });
+        btnEqual = initBtn("=", x[2], y[5], event -> {
+            if (Pattern.matches("([-]?\\d+[.]\\d*)|(\\d+)", inText.getText()))
+                if (go) {
+                    val = calc(val, inText.getText(), opt);
+                    if (Pattern.matches("[-]?[\\d]+[.][0]*", String.valueOf(val))) {
+                        inText.setText(String.valueOf((int) val));
+                    } else {
+                        inText.setText(String.valueOf(val));
+                    }
+                    opt = '=';
+                    addWrite = false;
+                }
+        });
+        int windowWidth = window.getWidth();
+        int x1 = windowWidth - BUTTON_WIDTH; // 40 is a margin from the right edge
+        int y1 = 10; // 10 is a margin from the top edge
+        alwaysOnTop = initBtn("^", x1, y1, event -> {
+            repaintFont();
+            boolean isOnTop = window.isAlwaysOnTop();
+            window.setAlwaysOnTop(!isOnTop);
+        });
+        alwaysOnTop.setSize(BUTTON_WIDTH - 40, BUTTON_HEIGHT - 40);
+        btnEqual.setSize(2 * BUTTON_WIDTH + 10, BUTTON_HEIGHT);
+        final Color equalColor = new Color(211, 211, 211);
+        btnEqual.setBackground(equalColor);
+        window.setLayout(null);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close button clicked? = End The process
+        window.setVisible(true);
+    }
+    private RoundedButton initBtn(String label, int x, int y, ActionListener event) {
+        RoundedButton btn = new RoundedButton(label);
+        btn.setBorder(null);
+        btn.setBounds(x, y, BUTTON_WIDTH, BUTTON_HEIGHT);
+        btn.setFont(new Font("Tahoma", Font.PLAIN, 28));
+        btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btn.addActionListener(event);
+        btn.setFocusable(false);
+        final Color primaryDarkColor = new Color(50, 50, 50);
+        final Color txtcolor = new Color(160, 160, 160);
+        window.getContentPane().setBackground(null);
+        btn.setBackground(primaryDarkColor);
+        btn.setForeground(txtcolor);
+
+        window.add(btn);
+
+        return btn;
+    }
+    public double calc(double x, String input, char opt) {
+        inText.setFont(inText.getFont().deriveFont(Font.PLAIN));
+        double y = Double.parseDouble(input);
+        switch (opt) {
+            case '+' -> {
+                return x + y;
+            }
+            case '-' -> {
+                return x - y;
+            }
+            case '*' -> {
+                return x * y;
+            }
+            case '/' -> {
+                return x / y;
+            }
+            case '%' -> {
+                return x % y;
+            }
+            case '^' -> {
+                return Math.pow(x, y);
+            }
+            default -> {
+                inText.setFont(inText.getFont().deriveFont(Font.PLAIN));
+                return y;
+            }
+        }
+    }
+    public class RoundedButton extends JButton {
+        private Color backgroundColor;
+        public RoundedButton() {
+            this(null);
+        }
+        public RoundedButton(String text) {
+            super(text);
+            setOpaque(false);
+            backgroundColor = new Color(50, 50, 50);
+        }
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setColor(backgroundColor);
+            g2.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+            g2.dispose();
+            super.paintComponent(g);
+        }
+
+        public void setBackgroundColor(Color backgroundColor) {
+            this.backgroundColor = backgroundColor;
+        }
+    }
+    private void repaintFont() {
+        inText.setFont(inText.getFont().deriveFont(Font.PLAIN));
+    }
+    public static void main(String[] args) {
+        new Calculator();
+    }
+}
