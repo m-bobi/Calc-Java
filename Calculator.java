5b619d6f20cec1d31c698be4920bfb088ba46647
siixty <melosbobi11@gmail.com>
2023-02-27 09:31:25 +0100

    +

--- Calculator.java
+++ Calculator.java
@@ -1,87 +1,153 @@
-import java.awt.Cursor;
-import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.util.regex.Pattern;
-import java.awt.Color;
 import javax.swing.*;
 import java.lang.Math;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 public class Calculator {
 
-    private static final int WINDOW_WIDTH = 403;
-    private static final int WINDOW_HEIGHT = 600;
-    private static final int BUTTON_WIDTH = 80;
-    private static final int BUTTON_HEIGHT = 70;
-    private static final int MARGIN_X = 20;
-    private static final int MARGIN_Y = 60;
+    private static final int WINDOW_WIDTH = 480; // Here we define the width of the window
+    private static final int WINDOW_HEIGHT = 600; // Here we define the height of the window
+    private static final int BUTTON_WIDTH = 80; // Here we define the width of the buttons
+    private static final int BUTTON_HEIGHT = 70; // Here we define the height of the buttons
+    private static final int MARGIN_X = 20; // Here we define the default x
+    private static final int MARGIN_Y = 60; // Here we define the default y
 
     private JFrame window; // Main window
     private JTextField inText; // Input
     private RoundedButton btnC, btnBack, btnMod, btnDiv, btnMul, btnSub, btnAdd,
             btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,
-            btnPoint, btnEqual, alwaysOnTop;
+            btnPoint, btnEqual, btnPower, btnLog, btnRoot; // Our button declaration
 
     private char opt = ' '; // Save the operator
     private boolean go = true; // For calculate with Opt != (=)
     private boolean addWrite = true; // Connect numbers in display
     private double val = 0; // Save the value typed for calculation
 
+    static Point mouseDownCompCoords; // For a movable window
+
     public Calculator() {
+
+        // Adding the calculator main frame.
 
         window = new JFrame("Calculator");
         window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        window.setUndecorated(true);
         window.setLocationRelativeTo(null); // Move window to center
+
         final Color mainBack = new Color(32, 32, 32);
         window.setBackground(mainBack);
+
         ImageIcon img = new ImageIcon("icon/calculator.png");
         window.setIconImage(img.getImage());
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        // Declaring the main positions we're going to use
 
         int[] x = { MARGIN_X, MARGIN_X + 90, 200, 290, 380 };
         int[] y = { MARGIN_Y, MARGIN_Y + 100, MARGIN_Y + 180, MARGIN_Y + 260, MARGIN_Y + 340, MARGIN_Y + 420 };
 
-        inText = new JTextField("0");
-        inText.setBounds(x[0], y[0], 350, 70);
-        inText.setBackground(mainBack);
+        mouseDownCompCoords = null; // Setting movable window pos to null
 
+        // Adding the Text Field
+
+        inText = new JTextField("0");
+        inText.setBounds(x[0], y[0], 400, 70);
+        inText.setBackground(mainBack);
+        inText.setEditable(false);
         inText.setForeground(Color.white);
         inText.setFont(new Font("Tahoma", Font.PLAIN, 33));
         inText.setHorizontalAlignment(JTextField.RIGHT);
         inText.setBorder(null);
-
-        KeyListener listener = new KeyListener() {
-            @Override
-            public void keyPressed(KeyEvent event) {
-                char keyCode = event.getKeyChar();
-
-                if (Character.isDigit(keyCode)) {
-                    if (inText.getText().equals(" ")) {
-                        inText.setText(String.valueOf(keyCode));
-                    } else {
-                        inText.setText(inText.getText() + keyCode);
-                    }
-                }
-            }
-
-            @Override
-            public void keyTyped(KeyEvent e) {
-                // TODO Auto-generated method stub
-
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-                // TODO Auto-generated method stub
-
-            }
-        };
-        window.addKeyListener(listener);
-
         window.add(inText);
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // Define custom title bar
+        JPanel titleBar = new JPanel();
+        titleBar.setBounds(0, 0, WINDOW_WIDTH, 30);
+        titleBar.setBackground(Color.DARK_GRAY);
+
+        JLabel title = new JLabel("Calculator");
+        title.setForeground(Color.WHITE);
+        title.setFont(new Font("Tahoma", Font.PLAIN, 18));
+
+        RoundedButton closeBtn = new RoundedButton("X");
+        closeBtn.setFocusable(false);
+        closeBtn.setBackground(Color.RED);
+        closeBtn.setForeground(Color.WHITE);
+        closeBtn.setBorderPainted(false);
+        closeBtn.addActionListener(e -> System.exit(0));
+
+        RoundedButton onTop = new RoundedButton("Stick");
+        onTop.setFocusable(false);
+        onTop.setBackground(Color.RED);
+        onTop.setForeground(Color.WHITE);
+        onTop.setBorderPainted(false);
+        onTop.addActionListener(e -> {
+            repaintFont();
+            boolean isOnTop = window.isAlwaysOnTop();
+            window.setAlwaysOnTop(!isOnTop);
+            if (!isOnTop) {
+                window.setLocation(2000, 100);
+            } else {
+                window.setLocationRelativeTo(null);
+            }
+        });
+
+        titleBar.add(title);
+        titleBar.add(Box.createHorizontalGlue());
+        titleBar.add(closeBtn);
+        titleBar.add(onTop);
+        window.add(titleBar);
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        /*  Adding the events to make the window movabale after removing the default frame.
+         * Taken from https://www.daniweb.com/programming/software-development/threads/300248/make-a-jframe-draggable-by-it-s-client-area#post1705581
+         * All credits go to the respected owner
+         *
+         *
+        */
+        window.addMouseListener(new MouseListener() {
+            public void mouseReleased(MouseEvent e) {
+                mouseDownCompCoords = null;
+            }
+
+            public void mousePressed(MouseEvent e) {
+                mouseDownCompCoords = e.getPoint();
+            }
+
+            public void mouseExited(MouseEvent e) {
+            }
+
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            public void mouseClicked(MouseEvent e) {
+            }
+        });
+
+        window.addMouseMotionListener(new MouseMotionListener() {
+            public void mouseMoved(MouseEvent e) {
+            }
+
+            public void mouseDragged(MouseEvent e) {
+                Point currCoords = e.getLocationOnScreen();
+                window.setLocation(currCoords.x - mouseDownCompCoords.x, currCoords.y - mouseDownCompCoords.y);
+            }
+        });
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+        /* Here we will initialize our buttons */
 
         btnC = initBtn("C", x[0], y[1], event -> {
             repaintFont();
@@ -323,6 +389,56 @@ public class Calculator {
                     opt = '+';
                 }
         });
+
+        btnPower = initBtn("^", x[4], y[1], event -> {
+            repaintFont();
+            if (Pattern.matches("([-]?\\d+[.]\\d*)|(\\d+)", inText.getText()))
+                if (go) {
+                    val = calc(val, inText.getText(), opt);
+                    if (Pattern.matches("[-]?[\\d]+[.][0]*", String.valueOf(val))) {
+                        inText.setText(String.valueOf((int) val));
+                    } else {
+                        inText.setText(String.valueOf(val));
+                    }
+                    opt = '^';
+                    go = false;
+                    addWrite = false;
+                } else {
+                    opt = '^';
+                }
+        });
+
+        btnLog = initBtn("ln", x[4], y[2], event -> {
+            if (Pattern.matches("([-]?\\d+[.]\\d*)|(\\d+)", inText.getText()))
+                if (go) {
+                    val = Math.log(Double.parseDouble(inText.getText()));
+                    if (Pattern.matches("[-]?[\\d]+[.][0]*", String.valueOf(val))) {
+                        inText.setText(String.valueOf((int) val));
+                    } else {
+                        inText.setText(String.valueOf(val));
+                    }
+                    opt = 'l';
+                    addWrite = false;
+                }
+        });
+        btnLog.setSize(BUTTON_WIDTH, 2 * BUTTON_HEIGHT + 10);
+
+        btnRoot = initBtn("√", x[4], y[4], event -> {
+            if (Pattern.matches("([-]?\\d+[.]\\d*)|(\\d+)", inText.getText()))
+                if (go) {
+                    val = Math.sqrt(Double.parseDouble(inText.getText()));
+                    if (Pattern.matches("[-]?[\\d]+[.][0]*", String.valueOf(val))) {
+                        inText.setText(String.valueOf((int) val));
+                    } else {
+                        inText.setText(String.valueOf(val));
+                    }
+                    opt = '√';
+                    addWrite = false;
+                }
+        });
+        btnRoot.setSize(BUTTON_WIDTH, 2 * BUTTON_HEIGHT + 10);
+
+
         btnPoint = initBtn(".", x[0], y[5], event -> {
             repaintFont();
             if (addWrite) {
@@ -362,23 +478,19 @@ public class Calculator {
                     addWrite = false;
                 }
         });
-        int windowWidth = window.getWidth();
-        int x1 = windowWidth - BUTTON_WIDTH; // 40 is a margin from the right edge
-        int y1 = 10; // 10 is a margin from the top edge
-        alwaysOnTop = initBtn("^", x1, y1, event -> {
-            repaintFont();
-            boolean isOnTop = window.isAlwaysOnTop();
-            window.setAlwaysOnTop(!isOnTop);
-        });
-        alwaysOnTop.setSize(BUTTON_WIDTH - 40, BUTTON_HEIGHT - 40);
+
         btnEqual.setSize(2 * BUTTON_WIDTH + 10, BUTTON_HEIGHT);
-        final Color equalColor = new Color(211, 211, 211);
-        btnEqual.setBackground(equalColor);
+
         window.setLayout(null);
         window.setResizable(false);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close button clicked? = End The process
         window.setVisible(true);
     }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    /* Here is how we design our buttons, it extends the JButton class. */
+
     private RoundedButton initBtn(String label, int x, int y, ActionListener event) {
         RoundedButton btn = new RoundedButton(label);
         btn.setBorder(null);
@@ -397,6 +509,11 @@ public class Calculator {
 
         return btn;
     }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    /* Here we do the calculations, it supports addition, substraction, multiplication, division, modulo, and raised to the power */
     public double calc(double x, String input, char opt) {
         inText.setFont(inText.getFont().deriveFont(Font.PLAIN));
         double y = Double.parseDouble(input);
@@ -425,16 +542,25 @@ public class Calculator {
             }
         }
     }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    /* Here we're editing the JButton class, to make rounded edges on the buttons */
+
     public class RoundedButton extends JButton {
         private Color backgroundColor;
+
         public RoundedButton() {
             this(null);
         }
+
         public RoundedButton(String text) {
             super(text);
             setOpaque(false);
             backgroundColor = new Color(50, 50, 50);
         }
+
         @Override
         protected void paintComponent(Graphics g) {
             Graphics2D g2 = (Graphics2D) g.create();
@@ -449,9 +575,14 @@ public class Calculator {
             this.backgroundColor = backgroundColor;
         }
     }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
     private void repaintFont() {
         inText.setFont(inText.getFont().deriveFont(Font.PLAIN));
     }
+
     public static void main(String[] args) {
         new Calculator();
     }
